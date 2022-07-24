264316e87b45425222f47ccf0ef4ce540ff15167
Ulf Lilleengen <lulf@redhat.com>
2022-07-24 08:00:36 +0200

    Use templating engine

--- api/src/api.rs
+++ api/src/api.rs
@@ -1,11 +1,27 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use lazy_static::lazy_static;
 use paho_mqtt as mqtt;
 use serde::{Deserialize, Serialize};
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     Arc,
 };
+use tera::{Context, Tera};
+
+lazy_static! {
+    pub static ref TERA: Tera = {
+        let mut tera = match Tera::new("templates/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera.autoescape_on(vec![".html", ".sql"]);
+        tera
+    };
+}
 
 pub struct ApiServer {
     port: u16,
@@ -65,42 +81,56 @@ async fn service(
     device: String,
     state: Arc<AtomicBool>,
 ) -> Result<Response<Body>, anyhow::Error> {
-    let mut response = Response::new(Body::empty());
-    match (req.method(), req.uri().path()) {
+    let response = match (req.method(), req.uri().path()) {
         (&Method::GET, "/healthz") => {
-            *response.body_mut() = Body::from("{\"status\": \"OK\"}");
+            let body = Body::from("{\"status\": \"OK\"}");
+            Response::new(body)
         }
-        (&Method::GET, "/doorlock/state") => {
-            *response.body_mut() = Body::from(format!("{{\"locked\": {}}}", state.load(Ordering::SeqCst)));
+        (&Method::GET, "/") => {
+            let mut ctx = Context::new();
+            let locked = state.load(Ordering::SeqCst);
+            let action = if locked { "Unlock" } else { "Lock" };
+            let url = if locked { "/doorlock/unlock" } else { "/doorlock/lock" };
+            ctx.insert("action", action);
+            ctx.insert("url", url);
+
+            let body = Body::from(TERA.render("index.html", &ctx).unwrap().to_string());
+            Response::new(body)
         }
-        (&Method::PUT, "/doorlock/lock") => {
+        (&Method::POST, "/doorlock/lock") => {
             let topic = format!("command/{}/{}/lock", application, device);
             let command = serde_json::to_vec(&DeviceResponse { command: Command::Lock })?;
             let message = mqtt::Message::new(topic, &command[..], 1);
-            if let Err(e) = client.publish(message).await {
+            let body = if let Err(e) = client.publish(message).await {
                 log::warn!("Error publishing command back to device: {:?}", e);
-                *response.body_mut() = Body::from(format!("{{\"status\": \"ERROR\", \"description\": {:?}\"}}", e));
+                Body::from(format!("{{\"status\": \"ERROR\", \"description\": {:?}\"}}", e))
             } else {
-                *response.body_mut() = Body::from("{\"status\": \"OK\"}");
-            }
+                let ctx = Context::new();
+                Body::from(TERA.render("action.html", &ctx).unwrap().to_string())
+            };
+            Response::new(body)
         }
-        (&Method::PUT, "/doorlock/unlock") => {
+        (&Method::POST, "/doorlock/unlock") => {
             let topic = format!("command/{}/{}/lock", application, device);
             let command = serde_json::to_vec(&DeviceResponse {
                 command: Command::Unlock,
             })?;
             let message = mqtt::Message::new(topic, &command[..], 1);
-            if let Err(e) = client.publish(message).await {
+            let body = if let Err(e) = client.publish(message).await {
                 log::warn!("Error publishing command back to device: {:?}", e);
-                *response.body_mut() = Body::from(format!("{{\"status\": \"ERROR\", \"description\": {:?}\"}}", e));
+                Body::from(format!("{{\"status\": \"ERROR\", \"description\": {:?}\"}}", e))
             } else {
-                *response.body_mut() = Body::from("{\"status\": \"OK\"}");
-            }
+                let ctx = Context::new();
+                Body::from(TERA.render("action.html", &ctx).unwrap().to_string())
+            };
+            Response::new(body)
         }
         _ => {
+            let mut response = Response::new(Body::empty());
             *response.status_mut() = StatusCode::NOT_FOUND;
+            response
         }
-    }
+    };
     Ok(response)
 }
 
